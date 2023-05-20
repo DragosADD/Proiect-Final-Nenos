99f6a867cdbfb50370112c9ad689ee19529f0255
DragosADD <84331719+DragosADD@users.noreply.github.com>
2023-05-20 14:02:21 +0300

    Checkbox and routs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Container, Grid } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Grid,
+  Button,
+  Box,
+} from "@mui/material";
 // import TodoList from "./TodoList";
 import ModalUi from "./components/modal/ModalUi";
 import TodoItem from "./components/Layout/TodoItem";
+// import { Link } from "react-router-dom";
 
 export default function App() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -19,16 +28,43 @@ export default function App() {
     <div>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography
-            variant="h6"
-            component="h1"
-            sx={{ flexGrow: 1, marginLeft: { xs: "2rem", md: "2rem" } }}
-          >
-            Your To-Do App
-          </Typography>
+          <label>Your to do App</label>
+          <Container maxWidth="lg">
+            <Grid container alignItems="center" justifyContent="center">
+              <Grid item>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    ml: "-5rem",
+                  }}
+                >
+                  <Button component="a" href="/" color="inherit" sx={{ mx: 1 }}>
+                    All
+                  </Button>
+                  <Button
+                    component="a"
+                    href="/done"
+                    color="inherit"
+                    sx={{ mx: 1 }}
+                  >
+                    Done
+                  </Button>
+                  <Button
+                    component="a"
+                    href="/todo"
+                    color="inherit"
+                    sx={{ mx: 1 }}
+                  >
+                    To-Do
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" style={{ marginTop: "1rem" }}>
         <Grid
           container
           spacing={2}
--- src/components/Layout/TodoItemTemplate.js
+++ src/components/Layout/TodoItemTemplate.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton, Checkbox } from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTimeOutlined";
 import EditIcon from "@mui/icons-material/EditOutlined";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 
 const TodoItemTemplate = (props) => {
+  const isCheckbox = true;
   return (
     <Box
       sx={{
@@ -40,8 +41,26 @@ const TodoItemTemplate = (props) => {
           </IconButton>
         </Box>
       </Box>
-      <Box sx={{ alignSelf: "flex-end" }}>
-        <Typography variant="body1">{props.text}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          mr: 3,
+          justifyContent: "space-between",
+        }}
+      >
+        {isCheckbox && (
+          <Box sx={{ alignSelf: "flex-start" }}>
+            <Checkbox
+              checked={props.isChecked}
+              onChange={props.onCheckboxChange}
+            />
+          </Box>
+        )}
+
+        <Box sx={{ alignSelf: "flex-end" }}>
+          <Typography variant="body1">{props.text}</Typography>
+        </Box>
       </Box>
     </Box>
   );
